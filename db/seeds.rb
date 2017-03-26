10deefa138da5862cd17fcae7e87bcf2d21769de
Rem Stone <remstone7@gmail.com>
2017-03-26 08:17:30 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+50.times do
+  # ! raises error if there is problem w/ data
+  Post.create!(
+    # call from lib_random_data
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+
+posts = Post.all
+
+100.times do
+  Comment.create!(
+    # sameple - pick random post to associate com w/
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finsihed"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
