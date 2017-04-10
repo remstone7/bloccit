c75e783bb525ab3545e2da6c90c56bc194b7c429
Rem Stone <remstone7@gmail.com>
2017-04-10 01:42:32 +0000

    auth sign in

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   post 'users/confirm' => 'users#confirm'
+  
+  resources :sessions, only: [:new, :create, :destroy]
 
   # instead of get welcome/abc
   get 'about' => 'welcome#about'
