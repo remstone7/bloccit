1f9259ad47a367da63790ef262abb3997702f569
remstone7 <remstone7@gmail.com>
2017-04-23 11:29:56 -0400

    voting

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,13 +27,16 @@ topics = Topic.all
 
 50.times do
   # ! raises error if there is problem w/ data
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     # call from lib_random_data
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+
+  rand(1..5).times { post.votes.create!(value: [-1,1].sample, user: users.sample)}
 end
 
 25.times do
@@ -84,3 +87,4 @@ puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{SponsoredPost.count} sponsored posts created"
+puts "#{Vote.count} votes created"
